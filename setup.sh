3f407c36de5a846ba6d229db77142ff5d323d227
kendim3chii <119109234+kendim3chii@users.noreply.github.com>
2022-11-26 17:03:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install rclone static binary
+cd ./npm && npm install && yarn && cd..
 wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -qqj rclone-current-linux-amd64.zip
 rm -rf rclone-current-linux-amd64.zip
